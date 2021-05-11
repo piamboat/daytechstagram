28f0dab82380638da0fd0a06c040452dd76e9608
piamsak <piamsakboat@gmail.com>
2021-05-12 01:01:10 +0700

    finish backend

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, ValidationPipe, Param, ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserCredentialsDto } from './dto/user-credentials.dto';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
+import { Post as post } from 'src/posts/post.entity';
 
 @Controller('users')
 export class UsersController {
@@ -14,5 +17,19 @@ export class UsersController {
     @Post('/signin')
     signIn(@Body(ValidationPipe) userCredentailsDto: UserCredentialsDto): Promise<{ accessToken: string }> {
         return this.usersService.singIn(userCredentailsDto)
+    }
+
+    @Get('/:id')
+    getUserById(
+        @Param('id', ParseIntPipe) id: number,
+        ): Promise<User> {
+        return this.usersService.getUserById(id)
+    }
+
+    @Get('/:id/posts')
+    getPostsByUserId(
+        @Param('id', ParseIntPipe) id: number,
+        ): Promise<post> {
+        return this.usersService.getPostsByUserId(id)
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,11 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { UserCredentialsDto } from './dto/user-credentials.dto';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './jwt-payload.interface';
+import { User } from './user.entity';
+import { Post } from '../posts/post.entity';
 
 @Injectable()
 export class UsersService {
@@ -28,5 +30,31 @@ export class UsersService {
         const accessToken = await this.jwtService.sign(payload)
 
         return { accessToken }
+    }
+
+    async getUserById(
+        id: number,
+        ): Promise<User> {
+        const found = await this.userRepository.findOne({ where: { id } })
+
+        if (!found) {
+            throw new NotFoundException(`User with id: ${id} not found`)
+        }
+
+        delete found.posts
+        delete found.comments
+        return found
+    }
+
+    async getPostsByUserId(
+        id: number,
+        ): Promise<Post> {
+        const found = await this.userRepository.findOne({ where: { id } })
+
+        if (!found) {
+            throw new NotFoundException(`User with id: ${id} not found`)
+        }
+
+        return found.posts
     }
 }
