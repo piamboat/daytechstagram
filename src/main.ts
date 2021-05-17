c8b93ddac23c0d9cb2a73b638adede236bb00227
piamsak <piamsakboat@gmail.com>
2021-05-18 03:47:29 +0700

    config upload folder to be able to return image

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as express from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use('/upload', express.static(join(__dirname, '..', 'upload')));
   await app.listen(3000);
 }
 bootstrap();
